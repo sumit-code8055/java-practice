7d3182c737eb2096ec0664549f2e3ab2f81965d6
Sumit Tiwari <st4889771@gmail.com>
2026-09-09 15:38:37 -0600

    Create TakeInput.java

---
+++ coreJava/Day-01/TakeInput.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class TakeInput{
+public static void main(String args[]){
+     Scanner sc = new Scanner(System.in);
+System.out.print("Enter the Your name : ");
+String name=sc.nextLine();
+System.out.print("Welcome mr. :"+name);
+
+		}
+
+}
