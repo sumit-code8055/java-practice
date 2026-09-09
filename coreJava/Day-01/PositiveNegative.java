b36145e82030c1fa31e66e770b3336bcc83083c5
Sumit Tiwari <st4889771@gmail.com>
2026-09-09 15:53:13 -0600

    Create PositiveNegative.java

---
+++ coreJava/Day-01/PositiveNegative.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class PositiveNegative{
+      public static void main(String args[]){
+	  Scanner sc=new Scanner(System.in);
+	   System.out.print("Enter the number :");
+		int a=sc.nextInt();
+		if(a>0){
+		   System.out.println("Positive Number:");
+			}
+		else if(a==0){
+		   System.out.println("Number is zero:");
+			}
+
+		else{
+		     System.out.println("Negative Number:");
+	
+			}
+		}
+
+}
