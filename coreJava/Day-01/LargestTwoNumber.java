1334d91e13e9ee4e1e679d6adec47a2b73457f0c
Sumit Tiwari <st4889771@gmail.com>
2026-09-09 16:04:35 -0600

    Create LargestTwoNumber.java

---
+++ coreJava/Day-01/LargestTwoNumber.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class LargestTwoNumber{
+public static void main(String args[]){
+    	  Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the 1st number :");
+		int a=sc.nextInt();
+		System.out.print("Enter the 2nd number :");
+		int b=sc.nextInt();
+		if(a>b){
+			System.out.print(a+" : is largest number");
+				}
+		else{
+			System.out.print(b+" : is largest number");
+				}
+		}	
+
+}
