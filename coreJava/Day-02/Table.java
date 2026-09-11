90239a47fe63ff3fad204cb201568669bd01aa72
Sumit Tiwari <st4889771@gmail.com>
2026-09-10 20:02:20 -0600

    Create Table.java

---
+++ coreJava/Day-02/Table.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Table {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+		System.out.print("Enter  the number whose table you want to print  : ");
+                  int table= sc.nextInt();
+		for(int i=1;i<=10;i++){
+			System.out.println(table+" "+"X"+" "+i+" "+"="+" "+table*i);
+			}
+}
+}
