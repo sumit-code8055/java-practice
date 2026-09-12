e86e3c6bdf1bd7e3405f8d586c2b9c06544d86bd
Sumit Tiwari <st4889771@gmail.com>
2026-09-11 19:38:45 -0600

    Create EvenDigitSum.java

---
+++ coreJava/Day-03/EvenDigitSum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class EvenDigitSum {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+		System.out.print("Enter  the number : ");
+                  int num= sc.nextInt();
+		int sum=0;
+		while(num!=0){
+		int last=num%10;
+		if(last%2==0){
+		 sum=last+sum;	
+			
+			}
+		num=num/10;
+		}
+	System.out.println(sum);
+}
+
+}
