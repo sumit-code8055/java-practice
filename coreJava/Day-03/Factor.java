86bbe05501a2cf30b874f60b270ab2545b00439f
Sumit Tiwari <st4889771@gmail.com>
2026-09-11 19:51:48 -0600

    Create Factor.java

---
+++ coreJava/Day-03/Factor.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Factor {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+		System.out.print("Enter  the number : ");
+                  int num= sc.nextInt();
+			for(int i=1;i<=num;i++){
+				if(num%i==0){
+					System.out.print(i+" ");
+							}
+					
+						}	
+				}
+	
+		}
