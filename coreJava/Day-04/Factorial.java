acc93da2f4562b458c658fd2157ba9f259fe1a22
Sumit Tiwari <st4889771@gmail.com>
2026-09-12 20:35:00 -0600

    Create Factorial.java

---
+++ coreJava/Day-04/Factorial.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Factorial{
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+		System.out.print("Enter  the number : ");
+                  int num= sc.nextInt();
+			int fact=1;
+		for (int i = num; i > 0; i--) {
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial of " + num + " = " + fact);
+    }
+}
