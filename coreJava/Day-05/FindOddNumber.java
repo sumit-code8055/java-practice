c94f51e2f22300fc68519b844425155689558a97
Sumit Tiwari <st4889771@gmail.com>
2026-09-13 20:22:31 -0600

    Create FindOddNumber.java

---
+++ coreJava/Day-05/FindOddNumber.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class FindOddNumber{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the  Array Size:");
+		int n=sc.nextInt();
+		int arr[] = new int[n];
+		System.out.print("Enter the  "+ n +" Array Element:");
+			for(int i=0;i<arr.length;i++){
+			arr[i]=sc.nextInt();
+				}
+			int check=0;
+			for(int i=0;i<arr.length;i++){
+				if(arr[i]%2!=0){
+					check=check+1;
+						}
+
+			}
+			System.out.println("Odd Number of Array :"+check);
+}
+		}
