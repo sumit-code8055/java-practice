4f1a3306a9073256971f6ffe96ba1e6b9094ffb5
Sumit Tiwari <st4889771@gmail.com>
2026-09-13 20:13:01 -0600

    Create Average_Array.java

---
+++ coreJava/Day-05/Average_Array.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Average_Array{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the  Array Size:");
+		int n=sc.nextInt();
+		int arr[] = new int[n];
+		System.out.print("Enter the  "+ n +" Array Element:");
+			for(int i=0;i<arr.length;i++){
+			arr[i]=sc.nextInt();
+				}
+			int sum=0;
+			for(int i=0;i<arr.length;i++){
+			sum=arr[i]+sum;	
+
+			}
+			System.out.println("Average of Array :"+sum/n);
+}
+		}
