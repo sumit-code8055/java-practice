058675568900d48a3b1205ebe943817d5ccd1e2c
Sumit Tiwari <st4889771@gmail.com>
2026-09-13 19:52:57 -0600

    Create ArrayInput.java

---
+++ coreJava/Day-05/ArrayInput.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class ArrayInput{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int arr[] = new int[5];
+		System.out.print("Enter the 5 Array Element:");
+			for(int i=0;i<arr.length;i++){
+			arr[i]=sc.nextInt();
+				}
+
+			for(int i=0;i<arr.length;i++){
+
+			System.out.print(arr[i]+" ");
+
+			}
+
+}
+		}
